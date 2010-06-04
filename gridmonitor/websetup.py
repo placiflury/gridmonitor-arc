b2791f5bcf8f35d80e697f6b790c28a4ee106177
Placi Flury <placi.flury@switch.ch>
2010-06-04 14:34:43 +0000

    initializes/creates database tables

--- gridmonitor/websetup.py
+++ gridmonitor/websetup.py
@@ -12,3 +12,12 @@ def setup_config(command, filename, section, vars):
     """Place any commands to setup gridmonitor here"""
     conf = appconfig('config:' + filename)
     load_environment(conf.global_conf, conf.local_conf)
+    
+    # Load the models
+    from gridmonitor.model.acl import meta
+    meta.acl_metadata.bind = meta.engine
+    meta.acl_metadata.create_all(checkfirst=True)
+    
+    from gridmonitor.model.sft import sft_meta
+    sft_meta.metadata.bind = sft_meta.engine
+    sft_meta.metadata.create_all(checkfirst=True)
